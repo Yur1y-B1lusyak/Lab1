7ada5a2a8e830f175e48c48797c82c08811ce232
Yuriy <yuriy_bilusyak@knu.ua>
2024-06-17 13:02:46 +0300

    Add create grid function

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
 import pygame
 import sys
-import random
+
+
+# Ініціалізація Pygame
+pygame.init()
+
+# Налаштування
+WIDTH, HEIGHT = 800, 800
+ROWS, COLS = 20, 20
+MINE_COUNT = 40
+TILE_SIZE = WIDTH // COLS
+
+# Вікно
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Minesweeper")
+
